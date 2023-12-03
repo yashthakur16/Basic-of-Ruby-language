2b3bbdc84521971e2ee663e34ff580ce807d1a6a
Yash Thakur <117333139+yashthakur16@users.noreply.github.com>
2023-12-03 10:56:50 +0530

    Create Access control.rb
    Note Accessing protected and private will give error

---
+++ Access control.rb
@@ -0,0 +1,20 @@
+class Access
+  public
+  def method1
+    puts "This is method 1"
+  end
+  
+  private
+  def method2
+    puts "This is method 2"
+  end
+  
+  protected
+  def method3
+    puts "This is method 3"
+  end
+
+end
+
+obj1=Access.new
+obj1.method1
